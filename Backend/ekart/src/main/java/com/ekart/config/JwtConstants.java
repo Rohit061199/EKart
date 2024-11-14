76184150eab0f206b6277dc477dc9f5fea72aba3
Rohit Kovur <kovurrohit95@gmail.com>
2024-11-13 19:25:08 -0600

    Backend push. Spring Security upto Authentication

---
+++ Backend/ekart/src/main/java/com/ekart/config/JwtConstants.java
@@ -0,0 +1,8 @@
+package com.ekart.config;
+
+public class JwtConstants {
+	
+	public static final String SECRET_KEY="hjbkjbdjhhsbfjhsdfbjdhb";
+	public static final String JWT_HEADER="Authorization";
+
+}
